126d83f9981fbc39925c8c71b92da3252365aeba
Daniel <dwinkelmann@outlook.com>
2022-02-11 05:25:07 +0100

    feat: add sample

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,21 @@
 <template>
   <div>
-    <div class="p-4 space-y-2 bg-bluish-900 text-bluish-50 rounded-lg flex flex-col">
-      <h1 class="text-2xl text-bluish-200 font-semibold">
-        {{ t('counter-example') }}
-      </h1>
-      <h2 class="text-7xl font-bold">
-        {{ count }}
-      </h2>
-      <PasswordTile label="PasswordTile" value="cDSVKctQx71hCKnu" />
-      <SliderTile v-model="count" label="Slider Tile" :min="5" :max="35" />
-      <ToggleTile v-model="toggle" label="Toggle Label" title="Toggle Tile" />
-      <ToggleTile v-model="toggle" title="Toggle Tile 1" />
-      <ToggleTile v-model="toggle" title="Toggle Tile 2" />
-      <GenerateButton @click="toggle ? increment() : decrement()" />
+    <div class="p-2 w-full min-w-full max-w-3xl space-y-2 bg-bluish-900 text-bluish-50 rounded-lg flex flex-col">
+      <button @click="copy">
+        <PasswordTile label="Password" :value="password" />
+      </button>
+      <SliderTile v-model="length" :label="'Length: ' + length" :min="5" :max="35" />
+      <ToggleTile v-model="numbers" label="Settings" title="Number Characters" />
+      <ToggleTile v-model="uppercase" title="Uppercase Characters" />
+      <ToggleTile v-model="special" title="Special Characters" />
+      <GenerateButton @click="generate" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useCounterComposable } from '~/composables/useCounterComposable'
-
-const { count, increment, decrement } = useCounterComposable()
-
-const { t } = useI18n()
-
-const toggle = ref(false)
+import usePasswordGenerator from '~/composables/usePasswordGenerator'
+const { password, length, generate, numbers, special, uppercase, copy } = usePasswordGenerator()
 </script>
 
 <route lang="yaml">
