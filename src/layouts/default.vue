47a6a3e6648341388a8d67a7e3343a45b773818f
Daniel <dwinkelmann@outlook.com>
2022-02-11 05:31:35 +0100

    refactor: sample design

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
-  <main class="min-h-screen bg-bluish-900 flex flex-col items-center justify-center py-10 text-center ">
+  <main class="min-h-screen p-4 bg-bluish-900 flex flex-col items-center justify-center">
     <router-view />
-    <Footer />
-    <div class="mt-5 mx-auto text-slate-50 text-center opacity-25 text-sm">
-      [Default Layout]
-    </div>
+    <!--     <Footer /> -->
   </main>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,17 @@
 <template>
-  <div>
-    <div class="p-2 w-full min-w-full max-w-3xl space-y-2 bg-bluish-900 text-bluish-50 rounded-lg flex flex-col">
-      <button @click="copy">
-        <PasswordTile label="Password" :value="password" />
-      </button>
-      <SliderTile v-model="length" :label="'Length: ' + length" :min="5" :max="35" />
-      <ToggleTile v-model="numbers" label="Settings" title="Number Characters" />
-      <ToggleTile v-model="uppercase" title="Uppercase Characters" />
-      <ToggleTile v-model="special" title="Special Characters" />
-      <GenerateButton @click="generate" />
-    </div>
+  <div class="space-y-2 w-full max-w-xl text-bluish-50 flex flex-col">
+    <PasswordTile label="Password" :value="password" />
+    <SliderTile v-model="length" :label="'Length: ' + length" :min="5" :max="35" />
+    <ToggleTile v-model="numbers" label="Settings" title="Number Characters" />
+    <ToggleTile v-model="uppercase" title="Uppercase Characters" />
+    <ToggleTile v-model="special" title="Special Characters" />
+    <GenerateButton @click="generate" />
   </div>
 </template>
 
 <script setup lang="ts">
 import usePasswordGenerator from '~/composables/usePasswordGenerator'
-const { password, length, generate, numbers, special, uppercase, copy } = usePasswordGenerator()
+const { password, length, generate, numbers, special, uppercase } = usePasswordGenerator()
 </script>
 
 <route lang="yaml">
